e1ece73dfdf7770b36b58b0d69ff10ffce651f52
Aaron D Williams <aaron@aarondwilliams.org>
2011-02-07 20:26:52 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Packrapt::Application.config.secret_token = '34db6e88c1eef5bf4b08c1f3fe0440b3576f3797ee45083cbd238b542ab86e0bd470af449bf01ff45be60f2f30613c6087a7197fcdb5f0d62176bf9513a80c06'
